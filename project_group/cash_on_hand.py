453f3c97fa8a843c798fcd5077ee88120a4526df
vernonAlgo2 <s10239948@connect.np.edu.sg>
2023-01-25 21:01:28 +0800

    Cash On Hand_ GOOD 1

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -2,20 +2,25 @@ from pathlib import Path
 import csv
 fp = Path.cwd()/"project_group"/"csv_reports"/"cash_on_hand.csv"
 
-cash_on_hand = {}
+cash_on_hand_list = []
 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
-    
-    previous_day = []
-    previous_coh = []
-    for row in reader:
-        day = row[0]
-        coh = float(row[1])
+    for day, coh in reader:
+        empty_dict = dict()
+        empty_dict["Day"] = day
+        empty_dict["Cash On Hand"] = coh
+        cash_on_hand_list.append(empty_dict)
 
+fp_cwd = Path.cwd()/"project_group"/"test.csv"
+fp_cwd.touch()
 
-day_cluster, coh_cluster = [[day[0] for day in previous_day], [coh[1] for coh in previous_coh]]
+with fp_cwd.open(mode="w", encoding = "UTF-8", newline ="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Day", "Cash On Hand"])
+    for item in cash_on_hand_list:
+        writer.writerow([item["Day"], item["Cash On Hand"]])
 
 
 
