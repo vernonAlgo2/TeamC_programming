aee33f492417de2cd5f68ac7ad21d4a4bd48d54c
vernonAlgo2 <s10239948@connect.np.edu.sg>
2023-01-25 21:21:10 +0800

    coh v2

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -3,7 +3,7 @@ import csv
 fp = Path.cwd()/"project_group"/"csv_reports"/"cash_on_hand.csv"
 
 cash_on_hand_list = []
-
+# reads the csv file containing cash on hand from day 40 to 50
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
@@ -12,13 +12,13 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         empty_dict["Day"] = day
         empty_dict["Cash On Hand"] = coh
         cash_on_hand_list.append(empty_dict)
-
+# keeps track of the previous day's cash on hand
 previous_day = cash_on_hand_list[0]["Day"]
 previous_coh = float(cash_on_hand_list[0]["Cash On Hand"])
 
 fp_cwd = Path.cwd()/"project_group"/"test.txt"
 fp_cwd.touch()
-
+# writes cash defecits if any onto txt file
 with fp_cwd.open(mode="w", encoding = "UTF-8", newline ="") as file:
     for i in range(1, len(cash_on_hand_list)):
         current_day = cash_on_hand_list[i]["Day"]
