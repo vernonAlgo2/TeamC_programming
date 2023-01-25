d1159c9414e60ec7419bafd195e4b6ca8df79070
vernonAlgo2 <s10239948@connect.np.edu.sg>
2023-01-25 14:40:39 +0800

    third commit

--- project_group/cash_on_hand.py
+++ project_group/cash_on_hand.py
@@ -8,17 +8,14 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
     
-    day_cluster = []
-    coh_cluster = []
-    
+    previous_day = []
+    previous_coh = []
     for row in reader:
-        row[0] == "Day"
-        day_cluster.append([row[0]])
-        row[1] == "Cash On Hand (Accumulated) (USD)"
-        coh_cluster.append([row[1]])
+        day = row[0]
+        coh = float(row[1])
 
-day_cluster, coh_cluster = [[day[0] for day in day_cluster], [coh[1] for coh in coh_cluster]]
 
+day_cluster, coh_cluster = [[day[0] for day in previous_day], [coh[1] for coh in previous_coh]]
 
 
 
