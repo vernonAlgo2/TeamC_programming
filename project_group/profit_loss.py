f254609e324befd3e911eb482e38ae8a107c80f7
vernonAlgo2 <s10239948@connect.np.edu.sg>
2023-01-25 23:09:18 +0800

    profit and loss success 1

--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -1,3 +1,31 @@
 from pathlib import Path
 import csv
-fp = Path.cwd()/"project_group"/"csv_reports"/"profit-and-loss-usd.csv"
+fp = Path.cwd()/"project_group"/"csv_reports"/"MAB CSV"/"profit-and-loss-usd.csv"
+pnl_list = []
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader)
+    for day,sales,trading_profit, operating_expense, net_profit in reader:
+        empty_dict = dict()
+        empty_dict["Day"] = day
+        empty_dict["Sales"] = sales
+        empty_dict["Trading Profit"] = trading_profit
+        empty_dict["Operating Expense"] = operating_expense
+        empty_dict["Net Profit"] = net_profit
+        pnl_list.append(empty_dict)
+
+previous_day = pnl_list[0]["Day"]
+previous_profit = float(pnl_list[0]["Net Profit"])
+
+fp_cwd = Path.cwd()/"project_group"/"test.txt"
+fp_cwd.touch()
+# writes cash defecits if any onto txt file
+with fp_cwd.open(mode="a", encoding = "UTF-8", newline ="") as file:
+    for i in range(1, len(pnl_list)):
+        current_day = pnl_list[i]["Day"]
+        current_profit = float(pnl_list[i]["Net Profit"])
+        if current_profit < previous_profit:
+            difference = previous_profit - current_profit
+            file.write(f"[PROFIT DEFICIT] DAY: {current_day} , AMOUNT: USD{difference}\n")
+        previous_day = current_day
+        previous_profit = current_profit
