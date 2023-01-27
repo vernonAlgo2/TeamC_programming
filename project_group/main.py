d26363fb63f3485ec60a9b21d60ff299ac2bf5be
vernonAlgo2 <s10239948@connect.np.edu.sg>
2023-01-27 13:54:40 +0800

    main commit

--- project_group/main.py
+++ project_group/main.py
@@ -1,6 +1,10 @@
 import overheads,cash_on_hand,profit_loss
 
 def main():
+    """
+    - Imports the lists from 3 different modules: Overheads, Cash On Hand, Profit and Loss
+    - Function runs the automation and creates a file "summary_report.txt" to store the output
+    """
     overheads.overhead_list
     cash_on_hand.cash_on_hand_list
     profit_loss.pnl_list
