4032bd3616e898eebaaeafa351b072267665c8ed
vernonAlgo2 <s10239948@connect.np.edu.sg>
2023-01-27 14:12:53 +0800

    pnl tweak

--- project_group/profit_loss.py
+++ project_group/profit_loss.py
@@ -8,16 +8,11 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
     for day,sales,trading_profit, operating_expense, net_profit in reader:
-        empty_dict = dict()
-        empty_dict["Day"] = day
-        empty_dict["Sales"] = sales
-        empty_dict["Trading Profit"] = trading_profit
-        empty_dict["Operating Expense"] = operating_expense
-        empty_dict["Net Profit"] = net_profit
-        pnl_list.append(empty_dict)
+        # append the data direct to the pnl_list as a list
+        pnl_list.append([day, sales, trading_profit, operating_expense, net_profit])
 
-previous_day = pnl_list[0]["Day"]
-previous_profit = float(pnl_list[0]["Net Profit"])
+previous_day = pnl_list[0][0]
+previous_profit = float(pnl_list[0][4])
 
 fp_cwd = Path.cwd()/"summary_report.txt"
 # writes cash defecits onto file
@@ -26,8 +21,8 @@ with fp_cwd.open(mode="a", encoding = "UTF-8", newline ="") as file:
     # loop starts from the second element because the first element is being used as a reference point for comparison
     for index in range(1, len(pnl_list)):
         # Get current day and profit
-        current_day = pnl_list[index]["Day"]
-        current_profit = float(pnl_list[index]["Net Profit"])
+        current_day = pnl_list[index][0]
+        current_profit = float(pnl_list[index][4])
         # Check if current profit is less than the previous profit
         if current_profit < previous_profit:
             # Calculate the difference between the current and previous profit
